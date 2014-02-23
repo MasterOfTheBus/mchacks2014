78918664b083d0955fd0d68f90130c6fb1291cba
Sidney Ng <sidney.ng@mail.mcgill.ca>
2014-02-23 05:37:40 -0500

    Not sure how to create a JSON object from the javascript variable, so we're not sure how to sendMessage; deleting popup and query javascript because it is likely we won't need them; not sure how to continue the project

--- highlight.js
+++ highlight.js
@@ -1,12 +1,38 @@
 // Select the highlighted text
 var selectedText = '';
-function selectText(e) {
+function selectText() {
 	// get the selection
     selectedText = (document.all) ? document.selection.createRange().text : document.getSelection();
+	console.log("text: " + selectedText);
+
+	// Query Translation
+	makePopup();
+}
+
+function makePopup() {
+	// Make the popup -- probably won't work
+	/*
+	var translationGenerator = {
+	showTranslation: function() {
+		var trans = document.createElement('input');
+		document.body.appendChild(trans);
+	}*/
+}
 	
-	// do something with the selection
-	//document.getElementById('input').value = selectedText;	
+	// Get the translation
+	getTranslation();
+}
+
+function getTranslation() {
+	// work stuff
+	
+	// show the translation
+	showTranslation();
+}
+
+function showTranslation()  {
+	// 
 }
 
 document.onmouseup = selectText;
-if (!document.all) document.captureEvents(Event.MOUSEUP);
+
--- popup.js
+++
@@ -1,6 +0,0 @@
-var translationGenerator = {
-	showTranslation: function() {
-		var trans = document.createElement('input');
-		document.body.appendChild(trans);
-	}
-}
